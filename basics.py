ab49f3f812fe2fc1f26006ccb9abf4b4adbe4fc3
Benjamen Alford <benjamen.alford@gmail.com>
2021-02-14 13:19:31 -0600

    pies

---
+++ basics.py
@@ -0,0 +1,23 @@
+# Variables
+a_number = 0  # assumes integer / whole number
+b_number = 0.0  # force to float number ( has decimal )
+some_test = "text text text"
+
+# data structures
+list_of_items = []
+list_of_items_2 = [1, 2, 3, 4]
+list_of_numbers = [a_number, b_number]
+list_of_items = ["cherry", "apple", "bacon"]  # anything
+
+# dictionary
+a_dict = {}
+a_dict_2 = {"pie": "cherry"}
+a_dict['pie'] = "cherry"
+
+pies = []
+pie_prices = []
+pie_ingredients = []
+
+pies.append("cherry")
+pie_prices.append(1.99)
+pie_ingredients.append(["crust", "cherries"])
